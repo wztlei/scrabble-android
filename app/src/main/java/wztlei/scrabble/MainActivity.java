19dde31b2c191c45e8e81daddb61c5fd5278bd48
wztlei <w.lei.thunderbolt@gmail.com>
2018-07-16 23:39:41 -0400

    Correctly commit and push the completion of UI designs

--- app/src/main/java/wztlei/scrabble/MainActivity.java
+++ app/src/main/java/wztlei/scrabble/MainActivity.java
@@ -1,31 +1,33 @@
 package wztlei.scrabble;
 
-import android.content.Context;
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
-import wztlei.scrabble.TextFileNames;
-
-
 public class  MainActivity extends AppCompatActivity {
+
+    ArrayList<String> boardStrings;
+    HashMap<Integer, Square> boardButtonIDs;
+    int lastSquareClickedID;
 
     /**
      * Changes the height and width of each button in the grid of Scrabble squares
      * so that they are squares and together they completely fill the device screen
      */
-    protected void setButtonDimensions(){
+    protected void setButtonDimensions() {
 
         // Get the width of the device in pixels
         DisplayMetrics displayMetrics = new DisplayMetrics();
@@ -35,20 +37,19 @@ public class  MainActivity extends AppCompatActivity {
         // Get the table by ID
         TableLayout tableLayout = findViewById(R.id.table_scrabble_board);
 
-        // Go through every button in the Scrabble board
-        // getChildCount() is not used since there are items
-        // below the grid that should not be changed
-        for (int row = 0; row < 15; row++) {
+        // Go through every button in the Scrabble board and the tiles below
+        for (int row = 0; row < 16; row++) {
             TableRow tableRow = (TableRow)tableLayout.getChildAt(row);
 
-            for (int col = 0; col < 15; col++) {
+            for (int col = 0; col < tableRow.getChildCount(); col++) {
 
-                // Change the height and width of the button to make it a square
-                View view = tableRow.getChildAt(col);
-                if (view instanceof  Button) {
-                Button square = findViewById(view.getId());
-                square.getLayoutParams().height = displayWidth/15;
-                square.getLayoutParams().width = displayWidth/15;}
+                if (!(row == 15 && col == 8)) {
+                    // Change the height and width of the button to make it a square
+                    View view = tableRow.getChildAt(col);
+                    Button square = findViewById(view.getId());
+                    square.getLayoutParams().height = displayWidth / 15;
+                    square.getLayoutParams().width = displayWidth / 15;
+                }
             }
         }
     }
@@ -57,7 +58,7 @@ public class  MainActivity extends AppCompatActivity {
      * Sets the padding of the row of buttons displaying the tiles in the rack
      * so that they are centered on the screen and the width of a square below the board.
      */
-    protected void setTileRowPadding(){
+    protected void setTileRowPadding() {
 
         // Get the width of the device in pixels
         DisplayMetrics displayMetrics = new DisplayMetrics();
@@ -66,16 +67,16 @@ public class  MainActivity extends AppCompatActivity {
 
         // Get the tableRow and set its left padding
         TableLayout tableLayout = findViewById(R.id.table_scrabble_board);
+
+        // Convert from pixels to density-independent pixels
+        float pxToDPscale = getResources().getDisplayMetrics().density;
+        int _20DP = (int) (20*pxToDPscale + 0.5f);
+
         TableRow tableRow = (TableRow)tableLayout.getChildAt(15);
-        tableRow.setPadding(4 * displayWidth/15, displayWidth/15,
-                            0, displayWidth/15);
+        tableRow.setPadding(displayWidth/15, _20DP, _20DP, 0);
     }
 
-    /**
-     * Sets the colours of the buttons so that they reflect the colours of a Scrabble board
-     */
-    protected void setButtonColors(){
-
+    protected void readBoardStrings() {
         TextFileNames textFileNames = new TextFileNames();
         String boardFileName = textFileNames.boardFileName;
 
@@ -91,8 +92,7 @@ public class  MainActivity extends AppCompatActivity {
             System.out.println("IOException due to " + boardFileName);
         }
 
-
-        ArrayList<String> boardDataStrings = new ArrayList<>();
+        boardStrings = new ArrayList<>();
 
         // Ensure file is open
         if (boardDataFile != null) {
@@ -102,28 +102,64 @@ public class  MainActivity extends AppCompatActivity {
 
                 // Read a line from the text file
                 String line = boardDataFile.nextLine();
-                boardDataStrings.add(line);
-                System.out.println(line);
+                boardStrings.add(line);
             }
         }
+    }
+
+    /**
+     * Stores all the button IDs in the Scrabble board.
+     */
+    @SuppressLint("UseSparseArrays")
+    protected void storeButtonIDs() {
 
         // Get the table by ID
         TableLayout tableLayout = findViewById(R.id.table_scrabble_board);
 
+        // Initialize the array to store all the IDs
+        boardButtonIDs = new HashMap<>();
+
         // Go through every button in the Scrabble board
         // getChildCount() is not used since there are items
         // below the grid that should not be changed
-        for (int tableRowNum = 0; tableRowNum < 15; tableRowNum++) {
+        for (int tableRowNum = 0; tableRowNum < 16; tableRowNum++) {
 
             TableRow tableRow = (TableRow)tableLayout.getChildAt(tableRowNum);
 
-            for (int tableColNum = 0; tableColNum < 15; tableColNum++) {
+            for (int tableColNum = 0; tableColNum < tableRow.getChildCount(); tableColNum++) {
+
+                View view = tableRow.getChildAt(tableColNum);
+                Square sqr = new Square();
+                sqr.row = tableRowNum + 1;
+                sqr.col = tableColNum + 1;
+                boardButtonIDs.put(view.getId(), sqr);
+            }
+        }
+    }
+
+    /**
+     * Sets the colours of the buttons so that they reflect the colours of a Scrabble board.
+     */
+    protected void setButtonColors() {
+
+        // Get the table by ID
+        TableLayout tableLayout = findViewById(R.id.table_scrabble_board);
+
+        // Initialize the array to store all the IDs
+
+        // Go through every button in the Scrabble board and the tiles below
+        for (int tableRowNum = 0; tableRowNum < 16; tableRowNum++) {
+
+            TableRow tableRow = (TableRow)tableLayout.getChildAt(tableRowNum);
+
+            for (int tableColNum = 0; tableColNum < tableRow.getChildCount(); tableColNum++) {
 
                 View view = tableRow.getChildAt(tableColNum);
                 Button square = findViewById(view.getId());
+                char squareChar = boardStrings.get(tableRowNum+1).charAt(tableColNum+1);
 
                 // Change the background to the proper drawable resource
-                switch (boardDataStrings.get(tableRowNum+1).charAt(tableColNum+1)) {
+                switch (squareChar) {
                     case 'W': square.setBackgroundResource(R.drawable.triple_word_square);   break;
                     case 'w': square.setBackgroundResource(R.drawable.double_word_square);   break;
                     case 'L': square.setBackgroundResource(R.drawable.triple_letter_square); break;
@@ -146,8 +182,131 @@ public class  MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        readBoardStrings();
+        storeButtonIDs();
         setButtonDimensions();
         setButtonColors();
         setTileRowPadding();
+
+        Button button = findViewById(R.id.button3);
+        button.setText("BEST MOVE");
+        lastSquareClickedID = 0;
+
     }
+
+    public void onClickEnterBoardTile(View view) {
+
+        if (lastSquareClickedID != 0) {
+            Button boardSquare = findViewById(lastSquareClickedID);
+            EditText boardEditText = findViewById(R.id.edit_text_board);
+            String inputtedTileLetter = boardEditText.getText().toString();
+            boardSquare.setText(inputtedTileLetter);
+        }
+    }
+
+    /**
+     * Changes the border of the button to white
+     *
+     * @param buttonID the ID of the button whose border needs to be changed to white
+     */
+    protected void drawButtonWhiteBorder(int buttonID) {
+
+        Button boardSquare = findViewById(buttonID);
+
+        // Get the row and col number of the button pressed
+        int rowNum = boardButtonIDs.get(buttonID).row;
+        int colNum = boardButtonIDs.get(buttonID).col;
+        char squareChar = boardStrings.get(rowNum).charAt(colNum);
+
+        // Change the background of the current button to the proper drawable resource
+        // This will cause the button to have a black border
+        switch (squareChar) {
+            case 'W':
+                boardSquare.setBackgroundResource(R.drawable.triple_word_square);
+                break;
+            case 'w':
+                boardSquare.setBackgroundResource(R.drawable.double_word_square);
+                break;
+            case 'L':
+                boardSquare.setBackgroundResource(R.drawable.triple_letter_square);
+                break;
+            case 'l':
+                boardSquare.setBackgroundResource(R.drawable.double_letter_square);
+                break;
+            case '.':
+                boardSquare.setBackgroundResource(R.drawable.regular_square);
+                break;
+            case 't':
+                boardSquare.setBackgroundResource(R.drawable.tile_square);
+                break;
+        }
+    }
+
+    /**
+     * Changes the border of the button to black
+     *
+     * @param buttonID the ID of the button whose border needs to be changed to black
+     */
+    protected void drawButtonBlackBorder(int buttonID) {
+
+        Button boardSquare = findViewById(buttonID);
+
+        // Get the row and col number of the button pressed
+        int rowNum = boardButtonIDs.get(buttonID).row;
+        int colNum = boardButtonIDs.get(buttonID).col;
+        char squareChar = boardStrings.get(rowNum).charAt(colNum);
+
+        // Change the background of the current button to the proper drawable resource
+        // This will cause the button to have a black border
+        switch (squareChar) {
+            case 'W':
+                boardSquare.setBackgroundResource(R.drawable.triple_word_square_pressed);
+                break;
+            case 'w':
+                boardSquare.setBackgroundResource(R.drawable.double_word_square_pressed);
+                break;
+            case 'L':
+                boardSquare.setBackgroundResource(R.drawable.triple_letter_square_pressed);
+                break;
+            case 'l':
+                boardSquare.setBackgroundResource(R.drawable.double_letter_square_pressed);
+                break;
+            case '.':
+                boardSquare.setBackgroundResource(R.drawable.regular_square_pressed);
+                break;
+            case 't':
+                boardSquare.setBackgroundResource(R.drawable.tile_square_pressed);
+                break;
+        }
+    }
+
+    /**
+     * Function is called when a button in the Scrabble board is pressed
+     *
+     * @param view the view of the button pressed
+     */
+    public void onClickBoardButton (View view) {
+
+        int currButtonID = view.getId();
+        Button boardSquare = findViewById(currButtonID);
+
+        // Set the text of the input text box (to change a tile on the board)
+        // to the text currently on the button
+        EditText boardEditText = findViewById(R.id.edit_text_board);
+        boardEditText.setText(boardSquare.getText());
+        boardEditText.requestFocus();
+
+
+        // Change the borders of the last button pressed
+        if (lastSquareClickedID != 0) {
+            drawButtonWhiteBorder(lastSquareClickedID);
+        }
+
+        // Change the borders of the current button pressed
+        drawButtonBlackBorder(view.getId());
+
+        // Update the variable storing the ID of the last button pressed
+        lastSquareClickedID = view.getId();
+    }
+
 }
